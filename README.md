1b23c8b0e1f4ba2bfa2727ab69cf6763c7c964a6
haleyelder <haley.elder@live.com>
2019-09-28 22:08:24 -0700

    updated mongo and env

--- README.md
+++ README.md
@@ -3,5 +3,8 @@
 ### Goals
 base of [tutorial](https://zellwk.com/blog/crud-express-mongodb/) to get started (updated with ES6 goodness!)
 stack - MERN
-- mlab backend
+- mLab/Mongo backend
+    - note: pushed to use Mongo Atlas to connect, created new cluster/collection, added dotenv for env variables 
 - react front end
+
+----------------------------
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+const dotenv = require('dotenv');
+dotenv.config();
+
 const app = express()
 
 app.use(bodyParser.urlencoded({extended: true}))
-
-app.listen(3000, function() {
-    console.log('listening on 3000')
-})
 
 // app.get(path, callback); show index
 app.get('/', (req,res) => {
@@ -14,5 +14,24 @@ app.get('/', (req,res) => {
 })
 
 app.post('/shows', (req, res) => {
-    console.log(req.body)
+    db.collection('shows').save(req.body, (err, result) => {
+        if (err) return console.log(err)
+
+        console.log('saved to db')
+        res.redirect('/')
+    })
 })
+
+const uri = process.env.uri
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
+var db
+
+client.connect(err => {
+  //  if (err) return console.log(err)
+    db = client.db('shows')
+    app.listen(3000, () => {
+        console.log('listening on 3000')
+    })
+  client.close();
+});
