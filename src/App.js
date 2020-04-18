ebbab21dcdcd0624460df8de2f5c1e512e7f3e31
haleyelder <haley.elder@live.com>
2020-04-17 22:11:52 -0700

    refactor, display on page

--- src/App.js
+++ src/App.js
@@ -1,75 +1,43 @@
-import React from "react";
-import SearchBar from "./components/SearchBar"
-import UserList from './components/UserList'
-import AddTitle from "./components/AddTitleForms"
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
 
-import "./styles/styles.css"
+// import SearchBar from "./components/SearchBar";
+// import Movie from "./components/Movie";
+// import AddTitle from "./components/AddTitleForms";
 
+import "./styles/styles.css";
 
-const App = () => {
-  const user = 'Haley'
-  const titles = [
-    {genre: 'fantasy', year: 2017, type: 'tv', title: 'American Gods'},
-    {genre: 'sci-fi', year: 1977, type: 'movie', title: 'Star Wars: A New Hope'},
-    {genre: 'thriller', year: 2018, type: 'tv', title: 'Dark'},
-    {genre: 'drama', year: 2016, type: 'tv', title: 'Mr. Robot'},
-    {genre: 'sci - fi', year: 1995, type: 'movie', title: ''},
-  ];
-  // const title = [
-  //     {
-  //       // "id": 1,
-  //       "title": "American Gods",
-  //       "genre": "fantasy, adventure,drama",
-  //       "year": 2017,
-  //       "type": "tv",
-  //       // "seasons": 2,
-  //     },
-  //     {
-  //       // "id": 2,
-  //       "title": "Star Wars: A New Hope",
-  //       "genre": "sci - fi,fantasy, adventure",
-  //       "year": 1977,
-  //       "type": "movie",
-  //       // "seasons": null,
-  //     },
-  //     {
-  //       // "id": 3,
-  //       "title": "Dark",
-  //       "genre": "sci - fi, fantasy, thriller",
-  //       "year": 2018,
-  //       "type": "tv",
-  //       // "seasons": 2,
-  //     },
-  //     {
-  //       // "id": 4,
-  //       "title": "Mr. Robot",
-  //       "genre": "sci - fi, drama, thriller",
-  //       "year": 2016,
-  //       "type": "tv",
-  //       // "seasons": 4,
-  //     },
-  
-  //     {
-  //       // "id": 5,
-  //       "title": "The Fifth Element",
-  //       "genre": "sci - fi, fantasy",
-  //       "year": 1995,
-  //       "type": "movie",
-  //       // "seasons": null,
-  //     },
-  //   ];
-  
-  return (
-    <div>
-      <h2> Welcome, {user}</h2>
-      <SearchBar/>
-      <h3> {user}'s current list</h3>
-      <UserList titles={titles} />
-      <hr/>
-      <AddTitle/>
-    </div>
-  )
+const movieURL = "http://www.omdbapi.com/?t=American+Gods&apikey=7f07f413";
+
+class App extends React.Component {
+  state = {
+    titleData: {}
+  }
+
+  componentDidMount() {
+    axios.get(movieURL)
+    .then(res => res.data)
+    .then(res => {
+      this.setState({ titleData: res })
+    });
+  }
+
+  render() {
+    const {
+      Title, 
+      Genre
+    } = this.state.titleData
+
+    return (
+      <div>
+        <span>{Title}</span>
+        <br></br>
+        <span>{Genre}</span>
+        
+      </div>
+    )
+  }
+
 }
 
 export default App;
-
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React, {useState} from "react";
 
-const SearchBar = () => {
+const SearchBar = (props) => {
+  const [searchValue, setSearchValue] = useState("")
+
+  const handleSearchInput = (e) => {
+    setSearchValue(e.target.value);
+  }
+  
+  const resetInput = () => {
+    setSearchValue("")
+  }
+
+  const callSearch = (e) => {
+    e.preventDefault();
+    props.search(searchValue);
+    resetInput();
+  }
+  
+  
   return (
     <div>
-      <input type="text" placeholder="search!" />
+      <header>Watchlist!</header>
+
+      <h2>{props.text}</h2>
+
+      <form>
+        <input  
+          value={searchValue}
+          onChange={handleSearchInput}
+          type="text"
+        />
+        <input onClick={callSearch} type="submit" value="search"/>
+      </form>
     </div>
   );
 };
