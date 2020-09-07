0a3e11cf85ea9c46a259f5849aabdde5621be769
haleyelder <haley.elder@live.com>
2020-09-06 22:03:27 -0700

    update models, routes, client

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,26 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-import Navbar from './components/navbar.component.js'
-import TitlesList from './components/titles-list.component.js'
-import EditTitle from './components/edit-title.component.js'
-import CreateTitle from './components/create-title.component.js'
-import CreateUser from './components/create-user.component.js'
-
-
-const App = () => {
+function App() {
   return (
-    <Router>
-      <div className="container">
-        <Navbar />
-      </div>
-      <div>
-        <Route path="/" exact component={TitlesList} />
-        <Route path="/edit/:id" exact component={EditTitle} />
-        <Route path="/create" exact component={CreateTitle} />
-        <Route path="/user" exact component={CreateUser} />
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
--- server.js
+++ server.js
@@ -2,13 +2,20 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
-
+const path = require("path");
 const app = express();
+
 const port = process.env.PORT || 5000;
 
 //connect to DB
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+
+mongoose.connect(uri || "mongodb+srv://cluster0.rt7fw.mongodb.net/watchlist", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
 const connection = mongoose.connection;
 connection.once("open", () => {
   console.log("mongodb connected successfully");
@@ -30,3 +37,13 @@ app.use("/users", usersRouter);
 app.listen(port, () => {
   console.log(`server running on port: ${port}`);
 });
+
+// production 
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
